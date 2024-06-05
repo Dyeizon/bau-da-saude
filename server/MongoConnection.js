f0ada33ae9f01741a568452f77d3ab2e5ac1eb70
Dyeizon <dyeizonque@gmail.com>
2024-06-05 17:10:12 -0300

    server

---
+++ server/MongoConnection.js
@@ -0,0 +1,23 @@
+const {MongoClient, ServerApiVersion} = require('mongodb');
+const uri = process.env.MONGODB_KEY;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true
+    },
+});
+
+async function run() {
+    try {
+        await client.connect();
+        await client.db("bauDaSaude").command({ping: 1});
+        console.log("Ping.")
+    } finally {
+        console.log('Connection closed.')
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
+require('dotenv').config()
+require('./MongoConnection');
+
 const express = require('express')
 const app = express()
 const port = 3000
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
