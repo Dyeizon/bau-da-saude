8c98869f4c18fc87c05d7e148979435a2bad4d1e
Dyeizon <dyeizonque@gmail.com>
2024-06-09 20:35:29 -0300

    type fix

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -1,17 +1,17 @@
 'use client';
 
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef, RefObject } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Register() {
-    const [name, setName] = useState("");
-    const [cpf, setCpf] = useState("");
-    const [dataNasc, setDataNasc] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [showDateInput, setShowDateInput] = useState(false);
+    const [name, setName] = useState<string>("");
+    const [cpf, setCpf] = useState<string>("");
+    const [dataNasc, setDataNasc] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [showDateInput, setShowDateInput] = useState<boolean>(false);
 
-    const dateInputRef = useRef(null);
+    const dateInputRef = useRef<HTMLInputElement>(null);
     const router = useRouter();
 
     const handleDateSelect = () => {
@@ -22,13 +22,13 @@ export default function Register() {
         setShowDateInput(false);
     };
 
-    const handleClickOutside = (event) => {
-        if (dateInputRef.current && !dateInputRef.current.contains(event.target) && dataNasc === "") {
+    const handleClickOutside = (event: MouseEvent) => {
+        if (dateInputRef.current && !dateInputRef.current.contains(event.target as Node) && dataNasc === "") {
             setShowDateInput(false);
         }
     };
 
-    const formatDate = (dateString) => {
+    const formatDate = (dateString: string) => {
         const [year, month, day] = dateString.split("-");
         return `${day}/${month}/${year}`;
     };
