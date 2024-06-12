cb93131416d84f0e232f8f09023ec60974ece071
Dyeizon <dyeizonque@gmail.com>
2024-06-12 16:58:02 -0300

    .env

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
-const User = require('../models/user');
-const jwt = require('jsonwebtoken');
-const path = require('path');
 const cors = require('cors');
+
+const path = require('path');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
 const app = express();
