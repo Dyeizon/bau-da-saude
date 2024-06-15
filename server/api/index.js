dbe763a1f8dfa2d27a62bda0d4ae213405f35bc2
Dyeizon <dyeizonque@gmail.com>
2024-06-15 16:36:07 -0300

    origins

--- server/api/index.js
+++ server/api/index.js
@@ -16,11 +16,20 @@ const swaggerDocument = require("../api/swagger.json");
 
 const port = process.env.PORT || 3001;
 
+const allowedOrigins = ['http://localhost:3000', 'https://baudasaude.vercel.app', 'https://baudasaude-api.vercel.app'];
+
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
-
 app.use(express.json());
 app.use(cookieParser());
 
