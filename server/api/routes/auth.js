9de08ffa41076444e08e1b007776b6431c22cb37
Dyeizon <dyeizonque@gmail.com>
2024-06-15 17:04:30 -0300

    token to front

--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -25,7 +25,7 @@ router.post('/', async (req, res) => {
         const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
   
         res.cookie('token', token, { httpOnly: true, secure: process.env.NODE_ENV === 'production' });
-        res.status(200).send({ message: {'token': token} });
+        res.status(200).send({ token: token });
     } catch (error) {
         res.status(500).send(error);
     }
