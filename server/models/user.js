33647a4d34ed019b4036722479328f9c731adee0
heloziza <helogiacometti@gmail.com>
2024-06-15 21:24:50 -0300

    customCollectionName

--- server/models/user.js
+++ server/models/user.js
@@ -20,6 +20,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model('User', userSchema);
+const Users = mongoose.model('Users', userSchema, 'customCollectionName');
 
-module.exports = User;
+module.exports = Users;
