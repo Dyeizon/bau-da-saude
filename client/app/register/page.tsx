14235f22792930b9279b6685b30c8bc2419b47a4
heloziza <helogiacometti@gmail.com>
2024-06-29 21:38:38 -0300

    validação da senha e de outros campos no registro

--- client/app/register/page.tsx
+++ client/app/register/page.tsx
@@ -12,6 +12,10 @@ export default function Register() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [showDateInput, setShowDateInput] = useState<boolean>(false);
+  const [passwordError, setPasswordError] = useState<string>("");
+  const [nameError, setNameError] = useState<string>("");
+  const [emailError, setEmailError] = useState<string>("");
+  const [dataNascError, setDataNascError] = useState<string>("");
 
   const dateInputRef = useRef<HTMLInputElement>(null);
   const router = useRouter();
@@ -22,38 +26,93 @@ export default function Register() {
     return new Date(parseInt(year), parseInt(month)-1, parseInt(day));
   }
 
+  const validatePassword = (password: string): boolean => {
+    if (password.length < 8) {
+      return false;
+    }
+  
+    const uppercaseRegex = /[A-Z]/;
+    const lowercaseRegex = /[a-z]/;
+    const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+  
+    return (
+      uppercaseRegex.test(password) &&
+      lowercaseRegex.test(password) &&
+      specialCharRegex.test(password)
+    );
+  };
+  
+
   const handleFormSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-  
-    (async function async() {
-      try {
-        const birthDate = stringToDate(dataNasc);
-        const requestBody = JSON.stringify({ name, email, password, birthDate });
-        console.log('Request body:', requestBody);
-  
-        const response = await fetch(`${fetchUrl}/users`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: requestBody,
-          credentials: 'include'
-        });
-  
-        if (response.ok) {
-          const data = await response.json();
-          localStorage.setItem('token', data.token);
-          router.push('/');
-        } else {
-          const errorData = await response.json();
-          alert(errorData.error);
+
+    let formValid = true;
+
+    if (!name.trim()) {
+      setNameError("Campo nome é obrigatório.");
+      formValid = false;
+    } else {
+      setNameError("");
+    }
+
+    if (!email.trim()) {
+      setEmailError("Campo email é obrigatório.");
+      formValid = false;
+    } else {
+      setEmailError("");
+    }
+
+    if (!dataNasc.trim()) {
+      setDataNascError("Campo data de nascimento é obrigatório.");
+      formValid = false;
+    } else {
+      setDataNascError("");
+    }
+
+    if (!validatePassword(password)) {
+      setPasswordError(
+        "A senha deve conter 8 caracteres ou mais, tendo ao menos uma letra maiúscula, uma letra minúscula e um caractere especial."
+      );
+      formValid = false;
+    } else {
+      setPasswordError("");
+    }
+
+    if (formValid) {
+      (async function async() {
+        try {
+          const birthDate = stringToDate(dataNasc);
+          const requestBody = JSON.stringify({
+            name,
+            email,
+            password,
+            birthDate,
+          });
+
+          const response = await fetch(`${fetchUrl}/users`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: requestBody,
+            credentials: "include",
+          });
+
+          if (response.ok) {
+            const data = await response.json();
+            localStorage.setItem("token", data.token);
+            router.push("/");
+          } else {
+            const errorData = await response.json();
+            alert(errorData.error);
+          }
+        } catch (error) {
+          console.log("Erro no cadastro:", error);
+          alert("Erro no cadastro");
         }
-      } catch (error) {
-        console.log('Erro no cadastro:', error);
-        alert('Erro no cadastro');
-      }
-    })();
-  }  
+      })();
+    }
+  };
 
   const handleDateSelect = () => {
     setShowDateInput(true);
@@ -69,8 +128,6 @@ export default function Register() {
     const [year, month, day] = dateString.split("-");
     return `${day}/${month}/${year}`;
   };
-
-  
 
   useEffect(() => {
     if (showDateInput) {
@@ -94,7 +151,7 @@ export default function Register() {
                 <div className="h-screen g-0 lg:flex lg:flex-wrap">
                   <div className="flex items-center lg:w-6/12" style={{backgroundImage: 'url("/bau_register.jpg")', backgroundSize: 'cover'}}></div>
                   <div className="m-auto px-4 md:px-0 lg:w-6/12">
-                    <div className="md:mx-6 md:p-12">
+                    <div className="md:mx-6 md:p-6">
                       <div className="text-center">
                         <img className="mx-auto w-36" src="./bau.jpg" alt="logo" />
                         <h4 className="mb-6 mt-1 pb-1 text-xl font-semibold">
@@ -104,28 +161,28 @@ export default function Register() {
 
                       <form onSubmit={handleFormSubmit}>
                         <p className="mb-4">Registre-se já em nosso sistema!</p>
-                        <div className="relative mb-6" data-twe-input-wrapper-init>
+                        <div className="relative mb-4" data-twe-input-wrapper-init>
                           <input
                             type="text"
-                            className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none focus:outline-none focus:border-none focus:ring-0 transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+                            className={`peer block min-h-[auto] w-full rounded ${nameError ? 'border-red-500' : 'border-0'} bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none focus:outline-none focus:ring-0 transition-all duration-200 ease-linear ${name ? 'focus:placeholder:opacity-100 peer-focus:text-primary' : ''} dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary`}
                             id="name"
-                            placeholder="Name"
                             value={name}
                             onChange={e => setName(e.target.value)}
                           />
                           <label
                             htmlFor="name"
-                            className={`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out ${name ? '-translate-y-[0.9rem] scale-[0.8] text-primary bg-white rounded px-2' : 'peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-focus:bg-white peer-focus:rounded peer-focus:px-2'} peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary`}
+                            className={`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] transition-all duration-200 ease-out ${nameError ? 'text-red-500' : 'text-neutral-400'} ${name ? '-translate-y-[1rem] scale-[0.8] text-primary  bg-white rounded px-0.5' : 'peer-focus:-translate-y-[1rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-focus:bg-white px-0.5 peer-focus:rounded'} peer-data-[twe-input-state-active]:-translate-y-[1rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary`}
                           >
                             Nome
                           </label>
+                          {nameError && <p className="text-red-500 text-xs">{nameError}</p>}
                         </div>
 
-                        <div className="relative mb-6" data-twe-input-wrapper-init>
+                        <div className="relative mb-4" data-twe-input-wrapper-init>
                           {!showDateInput && (
                             <input
                               type="text"
-                              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+                              className={`peer block min-h-[auto] w-full rounded ${dataNascError ? 'border-red-500' : 'border-0'} bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none focus:outline-none focus:ring-0 transition-all duration-200 ease-linear ${dataNasc ? 'focus:placeholder:opacity-100 peer-focus:text-primary' : ''} dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary`}
                               id="dataNasc"
                               placeholder=""
                               value={dataNasc ? formatDate(dataNasc) : ""}
@@ -137,7 +194,7 @@ export default function Register() {
                             <input
                               type="date"
                               ref={dateInputRef}
-                              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+                              className={`peer block min-h-[auto] w-full rounded ${dataNascError ? 'border-red-500' : 'border-0'} bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none focus:outline-none focus:ring-0 transition-all duration-200 ease-linear ${dataNasc ? 'focus:placeholder:opacity-100 peer-focus:text-primary' : ''} dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary`}
                               id="dataNascInput"
                               value={dataNasc}
                               onChange={e => setDataNasc(e.target.value)}
@@ -145,45 +202,47 @@ export default function Register() {
                           )}
                           <label
                             htmlFor="dataNasc"
-                            className={`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out ${dataNasc || showDateInput ? '-translate-y-[0.9rem] scale-[0.8] text-primary bg-white rounded px-2' : 'peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-focus:bg-white peer-focus:rounded peer-focus:px-2'} peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary`}
+                            className={`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] transition-all duration-200 ease-out ${dataNascError ? 'text-red-500' : 'text-neutral-400'} ${dataNasc || showDateInput ? '-translate-y-[1rem] scale-[0.8] text-primary  bg-white rounded px-0.5' : 'peer-focus:-translate-y-[1rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-focus:bg-white px-0.5 peer-focus:rounded'} peer-data-[twe-input-state-active]:-translate-y-[1rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary`}
                           >
                             Data de Nascimento
                           </label>
+                          {dataNascError && <p className="text-red-500 text-xs">{dataNascError}</p>}
                         </div>
 
-                        <div className="relative mb-6" data-twe-input-wrapper-init>
+                        <div className="relative mb-4" data-twe-input-wrapper-init>
                           <input
                             type="email"
-                            className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none focus:outline-none focus:border-none focus:ring-0 transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+                            className={`peer block min-h-[auto] w-full rounded ${emailError ? 'border-red-500' : 'border-0'} bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none focus:outline-none focus:ring-0 transition-all duration-200 ease-linear ${email ? 'focus:placeholder:opacity-100 peer-focus:text-primary' : ''} dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary`}
                             id="email"
-                            placeholder="Email"
                             value={email}
                             onChange={e => setEmail(e.target.value)}
                           />
                           <label
                             htmlFor="email"
-                            className={`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out ${email ? '-translate-y-[0.9rem] scale-[0.8] text-primary bg-white rounded px-2' : 'peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-focus:bg-white peer-focus:rounded peer-focus:px-2'} peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary`}
+                            className={`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] transition-all duration-200 ease-out ${emailError ? 'text-red-500' : 'text-neutral-400'} ${email ? '-translate-y-[1rem] scale-[0.8] text-primary  bg-white rounded px-0.5' : 'peer-focus:-translate-y-[1rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-focus:bg-white px-0.5 peer-focus:rounded'} peer-data-[twe-input-state-active]:-translate-y-[1rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary`}
                           >
                             Email
                           </label>
+                          {emailError && <p className="text-red-500 text-xs">{emailError}</p>}
                         </div>
 
-                        <div className="relative mb-6" data-twe-input-wrapper-init>
+                        <div className="relative mb-4" data-twe-input-wrapper-init>
                           <input
                             type="password"
-                            className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none focus:outline-none focus:border-none focus:ring-0 transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
+                            className={`peer block min-h-[auto] w-full rounded ${passwordError ? 'border-red-500' : 'border-0'} bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none focus:outline-none focus:ring-0 transition-all duration-200 ease-linear ${password ? 'focus:placeholder:opacity-100 peer-focus:text-primary' : ''} dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary`}
                             id="password"
-                            placeholder="Senha"
                             value={password}
                             onChange={e => setPassword(e.target.value)}
                           />
                           <label
                             htmlFor="password"
-                            className={`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out ${password ? '-translate-y-[0.9rem] scale-[0.8] text-primary bg-white rounded px-2' : 'peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-focus:bg-white peer-focus:rounded peer-focus:px-2'} peer-data-[twe-input-state-active]:-translate-y-[0.9rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary`}
+                            className={`pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] transition-all duration-200 ease-out ${passwordError ? 'text-red-500' : 'text-neutral-400'} ${password ? '-translate-y-[1rem] scale-[0.8] text-primary  bg-white rounded px-0.5' : 'peer-focus:-translate-y-[1rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-focus:bg-white px-0.5 peer-focus:rounded'} peer-data-[twe-input-state-active]:-translate-y-[1rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary`}
                           >
                             Senha
                           </label>
+                          {passwordError && <p className="text-red-500 text-xs">{passwordError}</p>}
                         </div>
+
                         <div className="mb-6 pb-1 pt-1 text-center">
                           <button
                             className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-dark-3 transition duration-150 ease-in-out hover:shadow-dark-2 focus:shadow-dark-2 focus:outline-none focus:ring-0 active:shadow-dark-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
